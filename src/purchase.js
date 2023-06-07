6933ea52404c0a0eb769edd62ba47bd4a7f9eb17
Lado Asambadze <lado.asambadze1@gmail.com>
2023-06-07 14:06:25 +0400

    feat: add function to update already added purchase

--- src/purchase.js
+++ src/purchase.js
@@ -29,15 +29,23 @@ async function main() {
   const index = existingProducts.findIndex(
     (product) => product.id === response.productId
   );
+  const index2 = existingPurchase.findIndex(
+    (purchase) => purchase.productId === response.productId
+  );
 
-  if (index !== -1) {
+  if (index2 !== -1) {
+    existingPurchase[index2].quantity = response.quantity;
+    existingPurchase[index2].price = response.price;
+    await existingPurchase[index2].save();
+    console.log("Purchase updated on MongoDB!");
+  } else if (index !== -1) {
     const newPurchase = new Purchase({
       quantity: response.quantity,
       price: response.price,
       productId: response.productId,
     });
     await newPurchase.save();
-    console.log("Product saved to MongoDB!");
+    console.log("Purchase saved to MongoDB!");
   } else {
     console.log("Product ID does not exist");
   }
